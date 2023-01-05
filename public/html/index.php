8f5d9a004796bb70056bfac153880abb61e3c25d
Rafnas Pentacodes <rafnasp@pentacodes.in>
2023-01-05 10:26:57 +0530

    Recently Viewed Slider Added

--- public/html/index.php
+++ public/html/index.php
@@ -64,6 +64,77 @@
 </section>
 
 
+
+
+
+<!--Home Slider Start-->
+    <section class="homeSlider">
+        <div class="container">
+            <div class="row justify-content-between">
+                <div class="col-xl-5 col-md-6 col-sm-6 col-6 d-flex align-items-center ">
+                    <div class="homeSliderDetailsArea d-block w-100">
+                        <div class="homeSliderDetails ">
+                            <div class="item">
+                                <h1>
+                                    <strong>Art</strong> Unleashed
+                                </h1>
+                                <p>
+                                    We bring you beautiful wall art to inspire and get you thinking. Choose from a vast
+                                    range of hand-picked canvas art prints to fit any decor. Go bold or subtle, with
+                                    pieces that work in any space.
+                                </p>
+                                <a href="" class="primary_btn">
+                                    Shop Now
+                                </a>
+                            </div>
+                            <div class="item">
+                                <h1>
+                                    <strong>Art</strong> Collections
+                                </h1>
+                                <p>
+                                    We bring you beautiful wall art to inspire and get you thinking. Choose from a vast
+                                    range of hand-picked canvas art prints to fit any decor. Go bold or subtle, with
+                                    pieces that work in any space.
+                                </p>
+                                <a href="" class="primary_btn">
+                                    Shop Now
+                                </a>
+                            </div>
+                            <div class="item">
+                                <h1>
+                                    <strong>Shop By</strong> Themes
+                                </h1>
+                                <p>
+                                    We bring you beautiful wall art to inspire and get you thinking. Choose from a vast
+                                    range of hand-picked canvas art prints to fit any decor. Go bold or subtle, with
+                                    pieces that work in any space.
+                                </p>
+                                <a href="" class="primary_btn">
+                                    Shop Now
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-5 col-md-6 col-sm-6 col-6 homeSliderArea">
+
+                    <div class="homeSliderImages ">
+                        <div class="item">
+                            <img class="img-fluid" src="assets/images/slider/slider_01.jpg" alt="">
+                        </div>
+                        <div class="item">
+                            <img class="img-fluid" src="assets/images/slider/slider_02.jpg" alt="">
+                        </div>
+                        <div class="item">
+                            <img class="img-fluid" src="assets/images/slider/slider_03.jpg" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+<!--Home Slider End-->
+
 <div class="relatedProducts">
     <div class="container">
         <div class="row">
@@ -525,75 +596,6 @@
         </div>
     </div>
 </div>
-
-
-<!--Home Slider Start-->
-    <section class="homeSlider">
-        <div class="container">
-            <div class="row justify-content-between">
-                <div class="col-xl-5 col-md-6 col-sm-6 col-6 d-flex align-items-center ">
-                    <div class="homeSliderDetailsArea d-block w-100">
-                        <div class="homeSliderDetails ">
-                            <div class="item">
-                                <h1>
-                                    <strong>Art</strong> Unleashed
-                                </h1>
-                                <p>
-                                    We bring you beautiful wall art to inspire and get you thinking. Choose from a vast
-                                    range of hand-picked canvas art prints to fit any decor. Go bold or subtle, with
-                                    pieces that work in any space.
-                                </p>
-                                <a href="" class="primary_btn">
-                                    Shop Now
-                                </a>
-                            </div>
-                            <div class="item">
-                                <h1>
-                                    <strong>Art</strong> Collections
-                                </h1>
-                                <p>
-                                    We bring you beautiful wall art to inspire and get you thinking. Choose from a vast
-                                    range of hand-picked canvas art prints to fit any decor. Go bold or subtle, with
-                                    pieces that work in any space.
-                                </p>
-                                <a href="" class="primary_btn">
-                                    Shop Now
-                                </a>
-                            </div>
-                            <div class="item">
-                                <h1>
-                                    <strong>Shop By</strong> Themes
-                                </h1>
-                                <p>
-                                    We bring you beautiful wall art to inspire and get you thinking. Choose from a vast
-                                    range of hand-picked canvas art prints to fit any decor. Go bold or subtle, with
-                                    pieces that work in any space.
-                                </p>
-                                <a href="" class="primary_btn">
-                                    Shop Now
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-xl-5 col-md-6 col-sm-6 col-6 homeSliderArea">
-
-                    <div class="homeSliderImages ">
-                        <div class="item">
-                            <img class="img-fluid" src="assets/images/slider/slider_01.jpg" alt="">
-                        </div>
-                        <div class="item">
-                            <img class="img-fluid" src="assets/images/slider/slider_02.jpg" alt="">
-                        </div>
-                        <div class="item">
-                            <img class="img-fluid" src="assets/images/slider/slider_03.jpg" alt="">
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-<!--Home Slider End-->
 
 <!--Home Collection Start-->
     <section class="collectionsArea">
