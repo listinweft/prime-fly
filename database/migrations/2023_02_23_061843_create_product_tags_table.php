c2c83b700c0ba0d9a102d08a23fc82a42e07dd2d
afeef_dev <afeef@pentacode.in>
2023-02-23 16:16:30 +0530

    #migration files fized

--- database/migrations/2023_02_23_061843_create_product_tags_table.php
+++ database/migrations/2023_02_23_061843_create_product_tags_table.php
@@ -28,12 +28,12 @@ return new class extends Migration
      */
     public function down()
     {
-<<<<<<<< HEAD:database/migrations/2023_02_23_061843_create_product_tags_table.php
+
         Schema::dropIfExists('product_tags');
-========
+
         Schema::table('product_types', function (Blueprint $table) {
             $table->dropColumn('sort_order');
         });
->>>>>>>> 82adb98e97f364609680acd1ef03330ac9f95b65:database/migrations/2023_02_23_060907_add_columns_to_product_types.php
+
     }
 };
--- database/migrations/2023_02_23_061907_create_product_type_table.php
+++ database/migrations/2023_02_23_061907_create_product_type_table.php
@@ -28,12 +28,12 @@ return new class extends Migration
      */
     public function down()
     {
-<<<<<<<< HEAD:database/migrations/2023_02_23_061907_create_product_type_table.php
+
         Schema::dropIfExists('product_type');
-========
+
         Schema::table('shapes', function (Blueprint $table) {
             $table->dropColumn('sort_order');
         });
->>>>>>>> 82adb98e97f364609680acd1ef03330ac9f95b65:database/migrations/2023_02_23_061024_add_columns_to_shapes.php
+
     }
 };
