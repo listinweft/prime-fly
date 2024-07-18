1c52bbdc5105a0b98f9dd5d57b6352925f9d00d6
listinweft <listin@wefttechnologies.com>
2024-07-18 17:40:09 +0530

    sort_order

--- resources/views/Admin/includes/menus/admin_menu.blade.php
+++ resources/views/Admin/includes/menus/admin_menu.blade.php
@@ -188,6 +188,7 @@
 </li>
 
 
+
 <li class="nav-item">
                   <a href="{{url(Helper::sitePrefix().'location')}}"
                  class="nav-link {{ (Request::segment(2)=='location')?'active':'' }}">
