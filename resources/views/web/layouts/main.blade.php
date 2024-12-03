12c4207db3dbcd1206f04645ad794188fcd3f648
listinweft <listin@wefttechnologies.com>
2024-12-03 11:37:16 +0530

    last

--- resources/views/web/layouts/main.blade.php
+++ resources/views/web/layouts/main.blade.php
@@ -2,17 +2,17 @@
 <html lang="en">
 <head>
 <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="Primefly is an exclusive airport hospitality service provided by Speedwings. They provide services such as meet and greet, 
     parking, cloakroom, check-in assistance, and baby/elder sitting within the airport premises. ">
     <title>Primefly</title> 
-  
+   -->
     <meta name="title" content="{!! @$seo_data->meta_title !!}">
     <meta name="description" content="{!! @$seo_data->meta_description !!}"/>
     <meta name="keywords" content="{!! @$seo_data->meta_keyword !!}"/>
     <meta name="csrf-token" content="{{ csrf_token() }}"/> 
     <link href="{{ asset('frontend/css/bootstrap.min.css')}}" rel="stylesheet" />
-    <title>Primefly</title>
+    <title>{!! @$seo_data->meta_title !!}</title>
       <link href="{{ asset('frontend/css/bootstrap.min.css')}}" rel="stylesheet">
       <link rel="stylesheet" href="{{ asset('frontend/css/owl.carousel.min.css')}}">
       <link rel="stylesheet" href="{{ asset('frontend/css/owl.theme.default.min.css')}}">
